e3371bbe7249d0fa68c4da2619f98656f47cfc65
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:56:02 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="lodash" />
-import * as _ from 'lodash';
-declare function stripKeys(obj: {}, keys: string | string[], deep?: boolean): _.Dictionary<{}>;
-export default stripKeys;
